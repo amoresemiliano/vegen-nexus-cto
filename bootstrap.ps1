83167b9ba60f3049c1bfcdafabedefee0dbdae41
Emiliano Di Rosa Amores <emilianodirosa1@gmail.com>
2026-08-07 20:18:04 +0200

    Delete bootstrap.ps1

--- bootstrap.ps1
+++
@@ -1,8 +0,0 @@
-$ErrorActionPreference = "Stop"
-git init
-git branch -M main
-git remote remove origin 2>$null
-git remote add origin https://github.com/amoresemiliano/nexus-dev-os.git
-git add .
-git commit -m "chore: bootstrap Vegen Nexus Dev OS v1.0.0"
-git push -u origin main
